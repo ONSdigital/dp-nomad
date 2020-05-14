d4b3345faeac4ed38067d20e56df6646002966be
Rhiannon Griffiths <rhiannon.griffiths@ons.gov.uk>
2020-05-14 10:03:47 +0100

    breakout functionality to new funcation and add more tests

--- client.go
+++ client.go
@@ -4,7 +4,6 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"errors"
-	"fmt"
 	dphttp "github.com/ONSdigital/dp-net/http"
 	"io/ioutil"
 	"net/http"
@@ -18,46 +17,59 @@ type Nomad struct {
 	// Name string
 }
 
-func NewClient(NomadEndpoint, NomadCACert string, NomadTLSSkipVerify bool) (*Nomad, error){
+const prefix = "https://"
+var caCertPool *x509.CertPool
+
+func NewClient(nomadEndpoint, nomadCACert string, nomadTLSSkipVerify bool) (*Nomad, error){
 
 	dpHTTPClient := *dphttp.DefaultClient
 
-	if strings.HasPrefix(NomadEndpoint, "https://") {
-		fmt.Print("in if https")
-		var tlsConfig *tls.Config
-		if NomadCACert != "" {
-			fmt.Print("in nomad CA if")
-			caCertPool, _ := x509.SystemCertPool()
-			if caCertPool == nil {
-				caCertPool = x509.NewCertPool()
-			}
-
-			caCert, err := ioutil.ReadFile(NomadCACert)
-			if err != nil {
-				return nil, err
-			}
-			if !caCertPool.AppendCertsFromPEM(caCert) {
-				return nil, errors.New("failed to append ca cert to pool")
-			}
-
-			tlsConfig = &tls.Config{
-				RootCAs: caCertPool,
-			}
-		} else if NomadTLSSkipVerify {
-			fmt.Print("in else if TLS")
-
-			// no CA file => do not check cert  XXX DANGER DANGER XXX
-			tlsConfig = &tls.Config{
-				InsecureSkipVerify: true,
-			}
-		} else {
-			return nil, errors.New("invalid configuration with https but no CA cert or skip verification enabled")
+	if strings.HasPrefix(nomadEndpoint, prefix) {
+		tlsConfig, err := createTlsConfig(nomadCACert, nomadTLSSkipVerify)
+		if err != nil {
+			return nil, err
 		}
+
 		dpHTTPClient.HTTPClient.Transport = &http.Transport{TLSClientConfig: tlsConfig}
 	}
 	return  &Nomad{
 		Client: &dpHTTPClient,
-		URL:    NomadEndpoint,
+		URL:    nomadEndpoint,
 		// Name:   name,
 	}, nil
-} 
+}
+
+func createTlsConfig (nomadCACert string, nomadLSSkipVerify bool) (*tls.Config, error) {
+
+	if nomadCACert == "" && !nomadLSSkipVerify {
+		return nil, errors.New("invalid configuration with https but no CA cert or skip verification enabled")
+	}
+	if nomadCACert != "" {
+		if caCertPool == nil {
+			var err error
+			caCertPool, err = x509.SystemCertPool()
+			if err != nil {
+				return nil, err
+			}
+			if caCertPool == nil {
+				caCertPool = x509.NewCertPool()
+			}
+		}
+
+		caCert, err := ioutil.ReadFile(nomadCACert)
+		if err != nil {
+			return nil, err
+		}
+		if !caCertPool.AppendCertsFromPEM(caCert) {
+			return nil, errors.New("failed to append ca cert to pool")
+		}
+
+		return &tls.Config{
+			RootCAs: caCertPool,
+		}, nil
+	}
+	// no CA file => do not check cert  XXX DANGER DANGER XXX
+	return &tls.Config{
+		InsecureSkipVerify: true,
+	}, nil
+}
--- client_test.go
+++ client_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 
-// NewClient(NomadEndpoint, NomadCACert string, NomadTLSSkipVerify bool)
 func TestNewClient(t *testing.T) {
 	Convey("Given acceptable input values", t, func(){
 		c, err := NewClient("https://localhost:4646", "testdata/testCertFile", false)
@@ -28,6 +27,56 @@ func TestNewClientFails(t *testing.T) {
 				So(err, ShouldNotBeNil)
 				So(c, ShouldBeNil)
 				So(err.Error(), ShouldStartWith, "invalid configuration with https")
+			})
+		})
+	})
+}
+
+func TestNewClientHTTP(t *testing.T) {
+	Convey("Given valid input values", t, func(){
+		c, err := NewClient("http://localhost:4646", "", false)
+		Convey("When a new Client is created", func(){
+			Convey("Then a client is returned with no errors", func(){
+				So(err, ShouldBeNil)
+				So(c, ShouldNotBeNil)
+			})
+		})
+	})
+}
+
+func TestCreateTLSConfigCert(t *testing.T) {
+	Convey("Given valid input values", t, func(){
+		c, err := createTlsConfig("", true)
+		Convey("When a new config is created", func(){
+			Convey("Then config is returned with no errors", func(){
+				So(err, ShouldBeNil)
+				So(c, ShouldNotBeNil)
+			})
+		})
+	})
+}
+
+func TestCreateTLSConfigCertFails(t *testing.T) {
+	Convey("Given invalid input values", t, func(){
+		c, err := createTlsConfig("/does/not/exist", false)
+		Convey("When a new config is created", func(){
+			Convey("Then no config is returned with errors", func(){
+				So(err, ShouldNotBeNil)
+				So(c, ShouldBeNil)
+				So(err.Error(), ShouldStartWith, "open /does/not/exist: no such")
+			})
+		})
+	})
+}
+
+func TestCreateTLSConfigBadCertFails(t *testing.T) {
+	Convey("Given invalid input values", t, func(){
+		c, err := createTlsConfig("testdata/testBadCertFile", false)
+		Convey("When a new config is created", func(){
+			Convey("Then no config is returned with errors", func(){
+				So(err, ShouldNotBeNil)
+				So(c, ShouldBeNil)
+				So(err.Error(), ShouldStartWith, "failed to append ca cert to pool")
 			})
 		})
 	})
